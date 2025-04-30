4ebe29e82796beac01c8b9c59177118dc349b41e
Nitin Bhagat <119115476+nitinbhagat12@users.noreply.github.com>
2025-04-30 08:45:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typed", {
+    strings: ["Java Developer", "DevOps Enthusiast", "Penetration Tester", "Problem Solver"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true
+  });
+  
